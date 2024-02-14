8d61e3dae2f8cd38c27bd0adf863b32e73c1ec29
Kawino1998 <jeremykawino@gmail.com>
2024-02-14 13:45:19 +0300

    fixed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Author: JKawino
+ *
+ * Prints string to std output.
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
