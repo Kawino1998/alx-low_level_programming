f6286317a52f25f4458712894aff5d7625752870
Kawino1998 <jeremykawino@gmail.com>
2024-02-14 14:13:14 +0300

    ALX

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+/*
+ * File: 6-size.c
+ * Auth: JKawino
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints sizes of different data types
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char);
+	printf("Size of a int: %d byte(s)", sizeof(int);
+	printf("Size of a long int: %d byte(s)", sizeof(long int);
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int);
+	printf("Size of a float: %d byte(s)", sizeof(float);
+	return (0);
+}
