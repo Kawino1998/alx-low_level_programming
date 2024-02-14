1ae1917a81fbcd0d0ec0cd8b4818d7e81dab7089
Kawino1998 <jeremykawino@gmail.com>
2024-02-14 13:54:16 +0300

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * Author: JKawino
- *
+ * Author: JKawinoi
+ * main - Entry point
  * Prints string to std output.
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/*Return: Always 0 (Success)*/
 	return (0);
 }
