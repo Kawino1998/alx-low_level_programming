f7e9f7023303d8a2b1c7c6ba53706a339269e753
Kawino1998 <jeremykawino@gmail.com>
2024-02-14 14:44:42 +0300

    ALX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,13 +13,13 @@
 
 int main(void)
 {
-    int i = 0;
-    char A[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-    while(A[i] != '\0')
-    {
-        putchar(A[i]);
-        i++;
-    }
-    putchar('\n');
-    return (1);
+	int i = 0;
+	char A[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	while(A[i] != '\0')
+	{
+		putchar(A[i]);
+		i++;
+	}
+	putchar('\n');
+	return (1);
 }
