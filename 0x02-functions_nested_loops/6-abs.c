18a65fb25f68f62e035779fbf77e1432a1ade951
Kawino1998 <jeremykawino@gmail.com>
2024-02-19 14:51:55 +0300

    ALX

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+/*
+ * File: 6-abs.c
+ * Auth: JKawino
+ */
+
+#include "main.h"
+
+/**
+* int _abs - Returns the absolute value of int
+*
+*Return: abs value of int
+*/
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (i);
+	}
+
+	else if (i < 0)
+	{
+		return (i * -1);
+	}
+	return (0);
+}
