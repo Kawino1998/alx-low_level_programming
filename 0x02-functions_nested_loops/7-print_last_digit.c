fcd6bed44c3ca25c665d163919db048e4411c1eb
Kawino1998 <jeremykawino@gmail.com>
2024-02-20 09:59:42 +0300

    ALX

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: JKawino
+ */
+
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ *
+ * Returns the value of the last digit
+ *
+ */
+
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
