9a9eaf98271ad74d7524ca223ce50861cb57fb50
Kawino1998 <jeremykawino@gmail.com>
2024-02-20 11:23:09 +0300

    ALX

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+/*
+ * File: 8-24_hours.c
+ * Auth: JKawino
+ */
+
+#include "main.h"
+
+/**
+ *  jack_bauer - prints every minute of the day
+ *
+ * Returns 0
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i;
+
+	int j;
+
+	int k;
+
+	int l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 3; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(i + 48);
+					_putchar(j + 48);
+					_putchar(':');
+					_putchar(k + 48);
+					_putchar(l + 48);
+					if (l < 9)
+					{
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
