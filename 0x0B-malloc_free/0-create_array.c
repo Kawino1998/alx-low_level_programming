7357445d9d1f989a302c436d97dcd2475fb30a90
Kawino1998 <jeremykawino@gmail.com>
2024-03-11 10:23:23 +0300

    Changes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,23 +12,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	unsigned int i = 0;
-
-	if (size == 0)
-		return (NULL);
-
+    
+	unsigned int i;
+    
 	p = (char *) malloc(sizeof(char) * size);
-
-	if (p == NULL)
-		return (0);
-
-	while (i < size)
+    
+	if (size == NULL || p == NULL)
+	{
+		return(0);
+	}
+	for (i = 0; i < size; i++)
 	{
 		*(p + i) = c;
-		i++;
 	}
-
 	*(p + i) = '\0';
-
-	return (p);
+	return(p);
 }
